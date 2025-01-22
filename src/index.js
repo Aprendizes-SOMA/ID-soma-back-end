edb6736b71470c9d471b6a6ff1aa2923459a052c
avelando <avelarrodrigues89@gmail.com>
2025-01-22 07:54:37 -0300

    Configurando a CORS

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
+
+const corsOptions = {
+  origin: ['http://localhost:3000'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+app.use(cors(corsOptions));
+
 app.use(express.json());
 
 const collaboratorsRoutes = require('./routes/collaborator');
