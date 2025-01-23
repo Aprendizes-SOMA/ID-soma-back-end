b2099e3420a1321eb485514c768ed433750f26fc
arturmartins <arturmartinssilva078@gmail.com>
2025-01-23 10:14:41 -0300

    feat(collaborator.js): Adiciona rotas para busca de colaboradores por nome, CPF e ID

--- src/routes/collaborator.js
+++ src/routes/collaborator.js
@@ -5,11 +5,17 @@ const {
   listCollaborators,
   updateCollaborator,
   deleteCollaborator,
+  listCollaboratorsByName,
+  listCollaboratorsByCPF,
+  getCollaboratorById,
 } = require('../controllers/collaborator');
 
 router.post('/', addCollaborator);
 router.get('/', listCollaborators);
 router.put('/:id', updateCollaborator);
 router.delete('/:id', deleteCollaborator);
+router.get('/search-name', listCollaboratorsByName);
+router.get('/search-cpf', listCollaboratorsByCPF);
+router.get('/:id', getCollaboratorById);
 
 module.exports = router;
