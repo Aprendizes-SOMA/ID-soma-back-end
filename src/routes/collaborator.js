dbbfd344c5add0f4b67b8196e5c6fd651d0739af
avelando <avelarrodrigues89@gmail.com>
2025-03-11 10:30:05 -0300

    Alterações

--- src/routes/collaborator.js
+++ src/routes/collaborator.js
@@ -16,9 +16,9 @@ router.post('/', authenticateToken, addCollaborator);
 router.get('/', authenticateToken, listCollaborators);
 router.put('/:id', authenticateToken, updateCollaborator);
 router.delete('/:id', authenticateToken, deleteCollaborator);
+router.get('/exact-name', getCollaboratorByExactName);
 router.get('/search-name', listCollaboratorsByName);
 router.get('/search-cpf', listCollaboratorsByCPF);
 router.get('/:id', authenticateToken, getCollaboratorById);
-router.get("/exact-name", getCollaboratorByExactName);
 
 module.exports = router;
